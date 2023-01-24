590ea6d2db5f16459ca21911f3f91e07a3e0959d
AnukaJapa <japaridze.ana96@gmail.com>
2023-01-24 15:27:30 +0100

    aggiunto search input e details

--- PizzeriaDiAnaJaparidze/Controllers/ArticoloController.cs
+++ PizzeriaDiAnaJaparidze/Controllers/ArticoloController.cs
@@ -12,17 +12,46 @@ namespace PizzeriaDiAnaJaparidze.Controllers
     [ApiController]
     public class ArticoloController : ControllerBase
     {
-                [HttpGet]
-        public IActionResult Get()
+        [HttpGet]
+        public IActionResult Get(string? search)
         {
             using (PizzeriaContext db = new PizzeriaContext())
             {
-                List<Pizza> articoli = db.Pizzas.ToList<Pizza>();
+                List<Pizza> articoli = new List<Pizza>();
+                if (search is null || search == "")
+                {
+                    articoli = db.Pizzas.ToList<Pizza>();
+                }
+                else
+                {
+                    articoli = db.Pizzas.Where(p => p.Title.ToLower().Contains(search.ToLower())).ToList<Pizza>();
+                }
+
                 return Ok(articoli);
             }
 
         }
+ //---------------------------------------------------------------------------------------
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            using (PizzeriaContext db = new PizzeriaContext())
+            {
+                Pizza articolo = db.Pizzas.Where(p => p.Id == id).FirstOrDefault();
+
+                if (articolo is null)
+                {
+                    return NotFound("L'articolo non è stato trovato con questo id");
+                }
+
+                return Ok(articolo);
+            }
+
+//---------------------------------------------------------------------------------------
+        }
+
 
     }
-}
 
+}
